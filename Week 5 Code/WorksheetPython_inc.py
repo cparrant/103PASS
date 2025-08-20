315b29b06ba3cba0b56e6e10fd797d352bbe1e55
Cole Parrant <cole.parrant@utas.edu.au>
2025-08-20 11:36:19 +1000

    Create WorksheetPython_inc.py

---
+++ Week 5 Code/WorksheetPython_inc.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 20 11:04:51 2025
+
+@author: Cole
+"""
+
+"""
+Question 1 - Given the predicate P(x) defined as x % 2 = 0, what is the result for the following values: 
+"""
+Set = { 1, 15, 4, 1828, 67, 42, 9, 0 }
+
+q1 = 0
+q1b = 0
+
+"""
+Question 2 - Given the below set, abits, what is the result of the following operations
+"""
+
+abits = 0b110101
+
+q2a = 0
+q2b = 0
+q2c = 0
+q2d = 0
+
+print(str(q1) + "\n" + str(q1b) + "\n")
+print(bin(q2a) + "\n" + bin(q2b) + "\n" + bin(q2c) + "\n" + bin(q2d))
