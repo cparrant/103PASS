18cd107fa09d59b5b35a2b7d12c2ce31783c3975
Cole Parrant <cole.parrant@utas.edu.au>
2025-08-27 11:36:26 +1000

    Create WorksheetPython_inc.py

---
+++ Week 6 Code/WorksheetPython_inc.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 20 11:04:51 2025
+
+@author: Cole
+"""
+
+"""
+Question 1 - Given the below set, abits, what is the result of the following operations
+"""
+
+abits = 0b110101
+
+q1a = 0
+q1b = 0
+q1c = 0
+q1d = 0
+
+print(bin(q1a) + "\n" + bin(q1b) + "\n" + bin(q1c) + "\n" + bin(q1d))
+
+"""
+Question 1 - From the questions in the worksheet, create functions to represent the logical equivalences pythonically
+"""
+
+q2a = 0
+q2b = 0
+q2c = 0
