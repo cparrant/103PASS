2dca382647083eead5fb7aece746dc541d22fbf9
Cole Parrant <cole.parrant@utas.edu.au>
2025-10-15 11:13:06 +1100

    Week 12 Push

---
+++ Week 12 Code/WorksheetPython_inc.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 10 11:04:51 2025
+
+@author: Cole
+"""
+
+import re
+
+"""
+Given the following text:
+
+"""
+
+match = "Located @ 10 Gormanston Rd.7009, Contact us on +61 481 119 729, Or by email at demo1997@provider.com. For further information, please visit our website at http://provider.com"
+
+
+"""
+Q3 – Create a Regular Expression to match only the email:
+
+"""
+
+email_pattern = ""
+
+"""
+Q4 – The phone number:
+
+"""
+
+phone_pattern = ""
+
+"""
+Q5 – The website:
+
+"""
+
+site_pattern = ""
+
+"""
+Q6 – and finally, all at once:
+
+"""
+
+### SOME LOGIC TO GET AND PRINT OUT THE RESULTS ###
